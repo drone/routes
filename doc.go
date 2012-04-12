a4078d1e9a0842cdd214adbf0512578799a4f2ba
Brad Rydzewski <brad.rydzewski@gmail.com>
2012-04-12 15:58:59 -0700

    added a default doc.go file

---
+++ doc.go
@@ -0,0 +1,33 @@
+/*
+Package routes a simple http routing API for the Go programming language,
+compatible with the standard http.ListenAndServe function.
+
+Create a new route multiplexer:
+
+	mux := routes.New()
+
+Define a simple route with a given method (ie Get, Put, Post ...), path and
+http.HandleFunc.
+
+	mux.Get("/foo", fooHandler)
+
+Define a route with restful parameters in the path:
+
+	mux.Get("/:foo/:bar", func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		foo := params.Get(":foo")
+		bar := params.Get(":bar")
+		fmt.Fprintf(w, "%s %s", foo, bar)
+	})
+
+The parameters are parsed from the URL, and appended to the Request URL's
+query parameters.
+
+To start the web server, use the standard http.ListenAndServe
+function, and provide the route multiplexer:
+
+    http.Handle("/", mux)
+    http.ListenAndServe(":8000", nil)
+
+*/
+package routes
