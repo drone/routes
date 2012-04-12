00ba098e858d3b291861ed1b89726a5abb52743b
Brad Rydzewski <brad.rydzewski@gmail.com>
2012-04-12 15:24:47 -0700

    added a ReadJson helper function

--- routes.go
+++ routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"encoding/json"
 	"encoding/xml"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -286,7 +287,6 @@ var DefaultAuthHandler = func(w http.ResponseWriter, r *http.Request) bool {
 // ServeJson replies to the request with a JSON
 // representation of resource v.
 func ServeJson(w http.ResponseWriter, v interface{}) {
-	//content, err := json.Marshal(v)
 	content, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -294,6 +294,18 @@ func ServeJson(w http.ResponseWriter, v interface{}) {
 	}
 	w.Write(content)
 	w.Header().Set("Content-Type", applicationJson)
+}
+
+// ReadJson will parses the JSON-encoded data in the http
+// Request object and stores the result in the value
+// pointed to by v.
+func ReadJson(r *http.Request, v interface{}) error {
+	body, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(body, v)
 }
 
 // ServeXml replies to the request with an XML
